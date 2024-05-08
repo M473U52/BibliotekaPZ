2e8b400ba7261d78350de0c393386649aaf58c6e
Palaszek <kuba.palaszewski8@gmail.com>
2024-05-08 06:00:34 +0200

    Email Confirmation done

--- Biblioteka/Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
+++ Biblioteka/Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
@@ -63,7 +63,7 @@ namespace Biblioteka.Areas.Identity.Pages.Account
             var user = await _userManager.FindByEmailAsync(Input.Email);
             if (user == null)
             {
-                ModelState.AddModelError(string.Empty, "Verification email sent. Please check your email.");
+                ModelState.AddModelError(string.Empty, "Potwierdzenie zostało wysłane. Sprawdź skrzynkę odbiorczą");
                 return Page();
             }
 
@@ -80,7 +80,7 @@ namespace Biblioteka.Areas.Identity.Pages.Account
                 "Confirm your email",
                 $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-            ModelState.AddModelError(string.Empty, "Verification email sent. Please check your email.");
+            ModelState.AddModelError(string.Empty, "Potwierdzenie zostało wysłane. Sprawdź skrzynkę odbiorczą");
             return Page();
         }
     }
