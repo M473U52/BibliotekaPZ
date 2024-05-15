68a7fa3c982be174e0000ca741d5fe504ec1542e
Palaszek <kuba.palaszewski8@gmail.com>
2024-05-15 03:11:37 +0200

    Footer

---
+++ Biblioteka/Pages/Shared/_UnderHeaderAboutPublishers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Biblioteka.Pages.Shared
+{
+    public class _UnderHeaderAboutPublishersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Biblioteka/Pages/Shared/_UnderHeaderAboutUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Biblioteka.Pages.Shared
+{
+    public class _UnderHeaderAboutUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
