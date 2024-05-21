b877fa534bd20860a7d26aceb07d7fecb5a118a7
Szymon Siłkin <szymonsin@gmail.com>
2024-05-22 00:33:15 +0200

    Dodanie modelu raportów oraz funkcji zbierającej podstawowe informacje.

---
+++ Biblioteka/Models/DailyRaport.cs
@@ -0,0 +1,48 @@
+﻿using Biblioteka.Context;
+using Biblioteka.Repositories;
+
+namespace Biblioteka.Models
+{
+    public class DailyRaport
+    {
+        private BorrowingRepository _borrowingRepository;
+        public int booksBorrowed { get; set; }
+        public int totalBorrowed { get; set; }
+
+        public DailyRaport(BibContext context)
+        {
+            _borrowingRepository = new BorrowingRepository(context);
+        }
+
+        public void getDataSince(DateTime initDate)
+        {
+            var allBorrowings = _borrowingRepository.GetAll();
+
+            var searchedBorrowings = new List<Borrowing>();
+            foreach (var borrow in allBorrowings)
+                if (borrow.borrowDate > initDate)
+                    searchedBorrowings.Add(borrow);
+
+            decimal totalPayments = 0;
+            int booksReturned = 0;
+            int booksPaid = 0;
+            List<Book> lostBooksList = new List<Book>();
+
+            foreach (var borrow in searchedBorrowings)
+            {
+                if (borrow.LateFee != null)
+                    totalPayments += borrow.LateFee;
+
+                if (borrow.IsReturned)
+                    booksReturned++;
+
+                if (borrow.IsPaid)
+                    booksPaid++;
+
+                if (borrow.bookLost)
+                    lostBooksList.Add(borrow.book);
+            }
+
+        }
+    }
+}
