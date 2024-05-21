3e7ed97626e4341e555de04530c96f9d379b2211
Szymon Siłkin <szymonsin@gmail.com>
2024-05-22 00:38:21 +0200

    2 funkcja

--- Biblioteka/Models/DailyRaport.cs
+++ Biblioteka/Models/DailyRaport.cs
@@ -42,5 +42,36 @@ namespace Biblioteka.Models
             }
 
         }
+
+        public void getDataBetween(DateTime initDate, DateTime endDate)
+        {
+            var allBorrowings = _borrowingRepository.GetAll();
+
+            var searchedBorrowings = new List<Borrowing>();
+            foreach (var borrow in allBorrowings)
+                if (borrow.borrowDate > initDate && borrow.borrowDate < endDate)
+                    searchedBorrowings.Add(borrow);
+
+            decimal totalPayments = 0;
+            int booksReturned = 0;
+            int booksPaid = 0;
+            List<Book> lostBooksList = new List<Book>();
+
+            foreach (var borrow in searchedBorrowings)
+            {
+                if (borrow.LateFee != null)
+                    totalPayments += borrow.LateFee;
+
+                if (borrow.IsReturned)
+                    booksReturned++;
+
+                if (borrow.IsPaid)
+                    booksPaid++;
+
+                if (borrow.bookLost)
+                    lostBooksList.Add(borrow.book);
+            }
+
+        }
     }
 }
