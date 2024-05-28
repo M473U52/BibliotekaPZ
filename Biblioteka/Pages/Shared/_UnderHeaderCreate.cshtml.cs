886ead4e8cd91af7503230c2568b6a17a0d3ca91
Palaszek <kuba.palaszewski8@gmail.com>
2024-05-28 20:33:05 +0200

    Events done

---
+++ Biblioteka/Pages/Shared/_UnderHeaderCreate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Biblioteka.Pages.Shared
+{
+    public class _UnderHeaderCreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
